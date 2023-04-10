d377d96bc0fee42e827d020906927ba98be9d8d5
hyeonjichoi21 <ch5i_hj15@naver.com>
2023-04-10 15:15:23 +0900

    4/6 수업 완료

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Todo from './Todo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props); // 부모 생성자를 반드시 호출해야 함
+    this.state = {
+      tiem: {id: 0, title: "Hello World 1", done: true}, // state에 item 객체를 담는다.
+    };
+  }
+
+  render(){
+    return (
+      <div className='App'>
+        <Todo item={this.state.item}/> 
+        <Todo /> 
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/Todo.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Todo extends React.Component {
+  render() {
+    return (
+      <div className="Todo">
+        <input type="checkbox" id="todo0" name='todo0' value='todo0'/>
+        <label for="todo0">Todo 컴포넌트 만들기</label>
+        {/*
+            comment here
+        */}
+        { 
+          // comment1
+          // comment2
+          // comment3
+         }
+         
+      </div>
+    );
+  }
+}
+
+export default Todo; 
