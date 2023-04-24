bd4ad0a08d0534d926d49a4a542d5bc0a69ee422
shchoi8347 <csh@duksung.ac.kr>
2023-04-24 16:00:59 +0900

    4/24 3.3절 프론트엔드 완료

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Todo from './Todo';
 import { Paper, List, Container } from "@material-ui/core";
 import AddTodo from "./AddTodo.js"
+import { call } from "./service/ApiService";
 
 class App extends React.Component {
   constructor(props) {
@@ -15,48 +16,27 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    const requestOptions = {
-      method: "GET",
-      headers: { "Content-Type": "application/json"},
-    };
-
-    fetch("http://localhost:8080/todo", requestOptions)
-      .then( (response) => response.json() )
-      .then(
-        (response) => {
-          this.setState( {
-            items: response.data,
-            });
-        },
-        (error) => {
-          this.setState({
-            error,
-          });
-        }
-      );
+    call("/todo", "GET", null).then((response) =>
+      this.setState({items: response.data})
+    );
   }
 
   add = (item) => {
-    const thisItems = this.state.items;
-    item.id = "ID-" + thisItems.length;
-    item.done = false;
-    thisItems.push(item);
-    this.setState({items: thisItems});
-
-    console.log("items: ", this.state.items);
-
+    call("/todo", "POST", item).then((response) =>
+      this.setState({ items: response.data })
+    );
   }
 
   delete = (item) => {
-    const thisItems = this.state.items;
+    call("/todo", "DELETE", item).then((response) =>
+      this.setState({ items: response.data })
+    );
+  }
 
-    console.log("Befere delete: ", this.state.items);
-
-    const newItems = thisItems.filter( e => e.id !== item.id);
-
-    this.setState({items: newItems}, () => {
-      console.log("After delete: ", this.state.items);
-    });
+  update = (item) => {
+    call("/todo", "PUT", item).then((response) => 
+      this.setState({items: response.data})
+    );
   }
 
   render() {
@@ -64,7 +44,12 @@ class App extends React.Component {
       <Paper style={{margin:16}}>
         <List>
           { this.state.items.map((item, index) => (
-              <Todo item={item} key={item.id} delete={this.delete} />
+              <Todo 
+                item={item} 
+                key={item.id} 
+                delete={this.delete}
+                update={this.update} 
+              />
           ))}
         </List>
       </Paper>
