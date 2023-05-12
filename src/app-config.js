cfcba72a96c992bd9c90f7e6f73b365926b6d5ef
shchoi8347 <csh@duksung.ac.kr>
2023-05-12 10:15:53 +0900

    FE 완성

--- src/app-config.js
+++ src/app-config.js
@@ -4,9 +4,6 @@ const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
   backendHost = "http://localhost:8080";
-} else {
-  //backendHost = "http://prod-todo-backend.us-west-2.elasticbeanstalk.com";
-  backendHost = "http://csh01-todobackend.us-west-2.elasticbeanstalk.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
